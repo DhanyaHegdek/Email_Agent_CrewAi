21057018a0ef25890a1006b838f07bf975fa43a5
dhanyashreemh <dhanyashree@raakacloud.com>
2026-07-23 19:24:55 +0530

    initial commit

---
+++ 5_yaml.py
@@ -0,0 +1,7 @@
+from crewai import Agent, Crew, Process, Task, LLM
+from crewai.project import CrewBase, agent, crew, task
+
+from crewai_tools import SerperDevTool, ScrapeWebsiteTool, DirectoryReadTool, FileWriterTool, FileReadTool
+
+from dotenv import load_dotenv
+load_dotenv()
